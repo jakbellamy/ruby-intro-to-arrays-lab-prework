ebd6817bc176381c1e5102968c19a9d4a0424285
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 01:51:16 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   ["element_1,", "element_2"]
 end
+
+def first_element(array)
+  array.last
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
